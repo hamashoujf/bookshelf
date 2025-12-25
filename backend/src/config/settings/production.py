5519cb70b1397dddf94b349d6e41c9eed974fcee
HamaShou74 <hamashou.jf@gmail.com>
2025-12-25 22:43:29 +0900

    feat(backend): initialize backend project

---
+++ backend/src/config/settings/production.py
@@ -0,0 +1,23 @@
+from .base import *
+
+
+env.read_env(env_file=BASE_DIR / '.env.development', overwrite=True)
+env.read_env(env_file=BASE_DIR / '.env.development.local', overwrite=True)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = env('SECRET_KEY')
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = env.bool("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': env.db()
+}
